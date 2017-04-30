f0993745fce4e2e28988006fc55a493e5184dbf7
Philip Hubbard <philiphubbardsoftware@gmail.com>
2017-04-30 09:53:41 -0700

    Initial Commit

---
+++ SGL/SGL.h
@@ -0,0 +1,19 @@
+//
+//  SGL.h
+//  SGL
+//
+//  Created by Philip on 4/30/17.
+//  Copyright © 2017 Philip Hubbard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGL.
+FOUNDATION_EXPORT double SGLVersionNumber;
+
+//! Project version string for SGL.
+FOUNDATION_EXPORT const unsigned char SGLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGL/PublicHeader.h>
+
+
